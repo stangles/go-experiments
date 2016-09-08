415c3ca47a57dc765a390f3aefe5f87659a4038c
Stangles <angles.steven@gmail.com>
2016-09-08 07:34:55 -0400

    completed concurrency

--- about_concurrency.go
+++ about_concurrency.go
@@ -10,18 +10,20 @@ func isPrimeNumber(possiblePrime int) bool {
 }
 
 func findPrimeNumbers(channel chan int) {
-	for i := 2; ; /* infinite loop */ i++ {
-		// your code goes here
-
-		assert(i < 100) // i is afraid of heights
+	for i := 2; ; i++ {
+		if isPrimeNumber(i) {
+			channel <- i
+		}
+		assert(i < 100)
 	}
 }
 
 func aboutConcurrency() {
 	ch := make(chan int)
 
-	assert(__delete_me__) // concurrency can be almost trivial
-	// your code goes here
+	go func() {
+		findPrimeNumbers(ch)
+	}()
 
 	assert(<-ch == 2)
 	assert(<-ch == 3)
